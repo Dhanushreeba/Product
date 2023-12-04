206d2717aee60f14b8c468973e0afab4880e44f7
Dhanushreeba <dhanushreeba.xworkz@gmail.comgit config --global user.name Dhanushreeba git config --global user.email dhanushreeba.xworkz@gmail.com>
2023-12-04 14:30:23 +0530

    hi

---
+++ DataTypes.java
@@ -0,0 +1,268 @@
+public class DataTypes{
+public static void main(String []args)
+{
+float piValue=3.141f;
+System.out.println(piValue);
+
+float sgpa=56.4f;
+System.out.println(sgpa);
+sgpa=7.42f;//reassingin
+System.out.println(sgpa);
+
+float fmRadioChannelNumber=98.3F;
+System.out.println(fmRadioChannelNumber);
+fmRadioChannelNumber=94.3F;//reassingin
+System.out.println(fmRadioChannelNumber);
+
+float studentMarks=65.5f;
+System.out.println(studentMarks);
+studentMarks=89.5f;//reassingin
+System.out.println(studentMarks);
+
+float cgpa=8.42f;
+System.out.println(cgpa);
+cgpa=9.42f;//reassingin
+System.out.println(cgpa);
+
+float bankBalance=500.50f;
+System.out.println(bankBalance);
+bankBalance=900.50f;//reassingin
+System.out.println(bankBalance);
+
+float studentPercentage=80.2f;
+System.out.println(studentPercentage);
+studentPercentage=98.5f;//reassingin
+System.out.println(studentPercentage);
+
+float kilometer=4.5f;
+System.out.println(kilometer);
+kilometer=7.5f;//reassingin
+System.out.println(kilometer);
+
+float weightOfAPerson=65.5f;
+System.out.println(weightOfAPerson);
+weightOfAPerson=45.5f;//reassingin
+System.out.println(weightOfAPerson);
+
+float height=6.5f;
+System.out.println(height);
+height=5.5f;//reassingin
+System.out.println(height);
+
+float circumference=6.25f;
+System.out.println(circumference);
+
+float mobileData=1.5f;
+System.out.println(mobileData);
+
+float radiusOfEarth=6.37f;
+System.out.println(radiusOfEarth);
+
+float instaFollowers=1.2f;
+System.out.println(instaFollowers);
+
+float phoneScreenTime=4.31f;
+System.out.println(phoneScreenTime);
+
+float amount=5.5f;
+System.out.println(amount);
+
+float instaLikes=1.5f;
+System.out.println(instaLikes);
+instaLikes=1.8f;
+System.out.println(instaLikes);
+
+//double
+
+double valueOfCircumferenceOfCircle=6.283185307d;
+System.out.println(valueOfCircumferenceOfCircle);
+
+double gravitationConstant=6.6743014;
+System.out.println(gravitationConstant);
+
+double exponentialConstant=2.178;
+System.out.println(exponentialConstant);
+
+double faradaysConstant=9.648533289d;
+System.out.println(faradaysConstant);
+
+double speedOfLight=2.99792458d;
+System.out.println(speedOfLight);
+
+double electronRestMass=9.10938356d;
+System.out.println(electronRestMass);
+
+double redbergConstant=1.0973731568505d;
+System.out.println(redbergConstant);
+
+double protonRestMass=1.672621898d;
+System.out.println(protonRestMass);
+
+double chargeOnElectron=1.6021766208d;
+System.out.println(chargeOnElectron);
+
+double neutronRestMass=1.674927471d;
+System.out.println(neutronRestMass);
+
+double boltzmann=1.38064852d;
+System.out.println(boltzmann);
+
+double plancksConstan=6.626070040d;
+System.out.println(plancksConstan);
+
+double fineStructureConstant=7.2973525664d;
+System.out.println(fineStructureConstant);
+
+//char
+
+char grade='A';
+System.out.println(grade);
+grade='B';//reassingin
+System.out.println(grade);
+
+char result='P';
+System.out.println(result);
+result='F';//reassingin
+System.out.println(result);
+
+char gender='M';
+System.out.println(gender);
+gender='F';//reassingin
+System.out.println(gender);
+
+char nameInitial='R';
+System.out.println(nameInitial);
+nameInitial='y';//reassingin
+System.out.println(nameInitial);
+
+char meter='M';
+System.out.println(meter);
+
+char length='L';
+System.out.println(length);
+
+char radius='R';
+System.out.println(radius);
+
+
+char multiplication='*';
+System.out.println(multiplication);
+multiplication='.';//reassingin
+System.out.println(multiplication);
+
+char division='/';
+System.out.println();
+
+char plus='+';
+System.out.println(plus);
+
+char minius='-';
+System.out.println(minius);
+
+char percentage='%';
+System.out.println(percentage);
+
+char asciiValueOfTen='A';
+System.out.println(asciiValueOfTen);
+
+char asciiValueOfEleven='B';
+System.out.println(asciiValueOfEleven);
+
+char asciiValueOfTwelve='C';
+System.out.println(asciiValueOfTwelve);
+
+char asciiValueOfThirteen='D';
+System.out.println(asciiValueOfThirteen);
+
+char asciiValueOfFourteen='E';
+System.out.println(asciiValueOfFourteen);
+
+char asciiValueOfFifteen='F';
+System.out.println(asciiValueOfFifteen);
+
+char ifItsCorrect='T';
+System.out.println(ifItsCorrect);
+
+char ifItsWrong='F';
+System.out.println(ifItsWrong);
+
+
+
+//boolean
+
+boolean indiaIsDevelopingCountry=true;
+System.out.println(indiaIsDevelopingCountry);
+
+boolean taskCompleted=false;
+System.out.println(taskCompleted);
+taskCompleted=true;//reassingin
+System.out.println(taskCompleted);
+
+boolean tommorowHoliday=false;
+System.out.println(tommorowHoliday);
+tommorowHoliday=true;//reassingin
+System.out.println(tommorowHoliday);
+
+boolean jogaFallssLocatedAtShivamoggaDistrict=true;
+System.out.println(jogaFallssLocatedAtShivamoggaDistrict);
+
+boolean thereAreSevenContinents=true;
+System.out.println(thereAreSevenContinents);
+
+boolean thereAreSevenRainbowColors=true;
+System.out.println(thereAreSevenRainbowColors);
+
+boolean bengaluruisKnownAsTheSiliconValleyOfIndia=true;
+System.out.println(bengaluruisKnownAsTheSiliconValleyOfIndia);
+
+boolean todaysdate=true;
+System.out.println(todaysdate);
+todaysdate=false;
+System.out.println(todaysdate);
+
+boolean sharksAreMammals=false;
+System.out.println(sharksAreMammals);
+
+boolean batsAreBlind=false;
+System.out.println(batsAreBlind);
+
+boolean southAfricaHasOneCapital=true;
+System.out.println(southAfricaHasOneCapital);
+
+boolean herbivoresAreAnimalEaters=false;
+System.out.println(herbivoresAreAnimalEaters);
+
+boolean octopusHasSevenHeart =false;
+System.out.println(octopusHasSevenHeart);
+
+boolean infantsHasMoreBonesThanAdults=true;
+System.out.println(infantsHasMoreBonesThanAdults);
+
+boolean humanBodyHasFourLungs=false;
+System.out.println(humanBodyHasFourLungs);
+
+boolean humanBrainIsMadeOfMuscle=false;
+System.out.println(humanBrainIsMadeOfMuscle);
+
+boolean humanBodyIsAboutSixtyPercentageWater=true;
+System.out.println(humanBodyIsAboutSixtyPercentageWater);
+
+
+boolean astronautHasPlayedGolfOnTheMoon=true;
+System.out.println(astronautHasPlayedGolfOnTheMoon);
+
+boolean fortuneCookiesWereInventedInChina=false;
+System.out.println(fortuneCookiesWereInventedInChina);
+
+boolean vaticanCityIsTheSmallestCityInTheWorld=true;
+System.out.println(vaticanCityIsTheSmallestCityInTheWorld);
+
+boolean hummingBirdIsTheWorldsSmallestBird=true;
+System.out.println(hummingBirdIsTheWorldsSmallestBird);
+
+boolean octopusHasEightHeart =true;
+System.out.println(octopusHasEightHeart);
+
+}
+}
+
