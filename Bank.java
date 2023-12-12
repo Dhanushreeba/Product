fc643ba825aed1ad38f9848a07c8568544b660ee
Dhanushreeba <dhanushreeba.xworkz@gmail.comgit config --global user.name Dhanushreeba git config --global user.email dhanushreeba.xworkz@gmail.com>
2023-12-12 20:06:26 +0530

    Method overloading

---
+++ Bank.java
@@ -0,0 +1,41 @@
+public class Bank{
+	public static void main(String args[]){
+balance(46,92.5f);
+withdraw(354,50.45f);
+deposit(700,56.2f);
+byte value=56;
+run(3.56f,value,'d');
+run(value,'g',23.6f);
+run('l',34.7f,value);
+run(567l,30,value);
+run('b',30.5f);
+run(34,23.7f,value,238l);
+	}
+static void balance(int input1,float input2){
+	System.out.println("This is 1st method");
+}
+static void withdraw(int input1,float input2){
+	System.out.println("This is 2nd method");
+}
+static void deposit(int input1,float input2){
+	System.out.println("This is 3rd method");
+}
+static void run(float input1,byte input2,char input3){
+	System.out.println("Execute the information");
+}
+static void run(byte input1,char input2,float input3){
+	System.out.println("Passing the value");
+}
+static void run(char input1,float input2,byte input3){
+	System.out.println("Code is executing");
+}
+static void run(long input1,int input2,byte input3){
+	System.out.println("Changing parameters");
+}
+static void run(char input1,float input2){
+	System.out.println("Account information");
+}
+static void run(int input1,float input2,byte input3,long input4){
+	System.out.println("Collecting information");
+}
+}
