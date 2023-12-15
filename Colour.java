d36d1a09ed18b8df49c1c5a961c16eb8ed02c3ff
Dhanushreeba <dhanushreeba.xworkz@gmail.comgit config --global user.name Dhanushreeba git config --global user.email dhanushreeba.xworkz@gmail.com>
2023-12-15 09:36:10 +0530

    for loops and if statment

---
+++ Colour.java
@@ -0,0 +1,196 @@
+public class Colour{
+	public static void main(String[] args){
+		
+		int[] numbers={12,31,76,34,54,95,23,98};
+		for(int i=numbers.length-1;i>=0;i--){
+			System.out.println(numbers[i]);
+		}
+		
+		int[] numbers1={12,31,76,34,54,95,23,98};
+		for(int i=(numbers.length-1)/2;i>=0;i--){
+			System.out.println( "middle" +numbers1[i]);
+		}
+		
+		int[] numbers2={12,31,76,34,54,95,23,98};
+		for(int i=(numbers.length+1)/2;i<numbers.length-1;i++){
+			System.out.println("mid" +numbers2[i]);
+		}
+		
+		int[] numbers3={12,31,76,34,54,95,23,98};
+		for(int i=0;i<numbers.length;i++){
+			if(numbers3[i]%2==0){
+				System.out.println("even" +numbers3[i]);
+			}
+		}
+		
+		int[] numbers4={12,31,76,34,54,95,23,98};
+		for(int i=0;i<numbers.length;i++){
+			if(numbers4[i]%2!=0){
+				System.out.println("odd" +numbers4[i]);
+			}
+		}
+		
+		int[] numbers5={12,31,76,34,54,95,23,98};
+		int sum=0;
+		for(int i=0;i<numbers.length;i++)
+			sum=sum+numbers5[i];
+			{
+				System.out.println("sum" +sum);
+			}
+			
+			
+        float[] cgpa={7.3452f,6.723f,9.05f,4.5667f,8.544f,5.089f,3.987f,2.54f,1.976f};
+		for(int i=cgpa.length-1;i>=0;i--){
+			System.out.println(cgpa[i]);
+		}
+		
+		float[] cgpa1={7.3452f,6.723f,9.05f,4.5667f,8.544f,5.089f,3.987f,2.54f,1.976f};
+		for(int i=(cgpa.length-1)/2;i>=0;i--){
+			System.out.println("middle" +cgpa[i]);
+		}
+		
+		float[] cgpa2={7.3452f,6.723f,9.05f,4.5667f,8.544f,5.089f,3.987f,2.54f,1.976f};
+		for(int i=(cgpa.length+1)/2;i<cgpa.length-1;i++){
+			System.out.println("mid" +cgpa[i]);
+		}
+		
+		float[] cgpa3={7.3452f,6.723f,9.05f,4.5667f,8.544f,5.089f,3.987f,2.54f,1.976f};
+		for(int i=0;i<cgpa.length;i++){
+			if(cgpa3[i]%2==0){
+				System.out.println("even" +cgpa3[i]);
+			}
+		}
+		
+		float[] cgpa4={7.3452f,6.723f,9.05f,4.5667f,8.544f,5.089f,3.987f,2.54f,1.976f};
+		for(int i=0;i<cgpa.length;i++){
+			if(cgpa4[i]%2!=0){
+				System.out.println("odd" +cgpa4[i]);
+			}
+		}
+		
+		float[] cgpa5={7.3452f,6.723f,9.05f,4.5667f,8.544f,5.089f,3.987f,2.54f,1.976f};
+		float sum1=0;
+		for(int i=0;i<cgpa5.length;i++){
+			sum1=sum1+cgpa5[i];
+			{
+				System.out.println("sum" +sum1);
+			}
+		}
+		
+		
+		double[] doubleArrays={96.76466,54.97576,68.53436,12.5433,38.9856,23.654,78.543,45.76543};
+		for(int i=doubleArrays.length-1;i>=0;i--){
+			System.out.println(doubleArrays[i]);
+		}
+		
+	    double[] doubleArrays1={96.76466,54.97576,68.53436,12.5433,38.9856,23.654,78.543,45.76543}; 
+		for(int i=(doubleArrays.length-1)/2;i>=0;i--){
+			System.out.println("middle" +doubleArrays[i]);
+		}
+		
+		double[] doubleArrays2={96.76466,54.97576,68.53436,12.5433,38.9856,23.654,78.543,45.76543};
+		for(int i=(doubleArrays.length+1)/2;i<doubleArrays.length-1;i++){
+			System.out.println("mid" +doubleArrays[i]);
+		}
+		
+		double[] doubleArrays3={96.76466,54.97576,68.53436,12.5433,38.9856,23.654,78.543,45.76543};
+		for(int i=0;i<doubleArrays.length;i++){
+			if(doubleArrays3[i]%2==0){
+				System.out.println("even" +doubleArrays3[i]);
+			}
+		}
+		
+		double[] doubleArrays4={96.76466,54.97576,68.53436,12.5433,38.9856,23.654,78.543,45.76543};
+		for(int i=0;i<doubleArrays.length;i++){
+			if(doubleArrays[i]%2!=0){
+				System.out.println("odd" +doubleArrays4[i]);
+			}
+		}
+		
+		double[] doubleArrays5={96.76466,54.97576,68.53436,12.5433,38.9856,23.654,78.543,45.76543};
+		double sum2=0;
+		for(int i=0;i<doubleArrays5.length;i++){
+			sum2=sum2+doubleArrays5[i];
+			{
+				System.out.println("sum" +sum2);
+			}
+		}
+		
+        short[] registerNumber={(short)1021,(short)1023,(short)1024,(short)1025,(short)1026,(short)1027,(short)1028,(short)1029};
+		for(int i=registerNumber.length-1;i>=0;i--){
+			System.out.println(registerNumber[i]);
+		}
+		
+		short[] registerNumber1={(short)1021,(short)1023,(short)1024,(short)1025,(short)1026,(short)1027,(short)1028,(short)1029};
+		for(int i=(registerNumber.length-1)/2;i>=0;i--){
+			System.out.println("middle" +registerNumber[i]);
+		}
+		
+		short[] registerNumber2={(short)1021,(short)1023,(short)1024,(short)1025,(short)1026,(short)1027,(short)1028,(short)1029};
+		for(int i=(registerNumber.length+1)/2;i<registerNumber.length-1;i++){
+			System.out.println("mid" +registerNumber[i]);
+		}
+		
+		short[] registerNumber3={(short)1021,(short)1023,(short)1024,(short)1025,(short)1026,(short)1027,(short)1028,(short)1029};
+		for(int i=0;i<registerNumber3.length;i++){
+			if(registerNumber[i]%2==0){
+				System.out.println("even" +registerNumber3[i]);
+			}
+		}
+		
+		short[] registerNumber4={(short)1021,(short)1023,(short)1024,(short)1025,(short)1026,(short)1027,(short)1028,(short)1029};
+		for(int i=0;i<registerNumber4.length;i++){
+			if(registerNumber[i]%2!=0){
+			System.out.println("odd" +registerNumber4[i]);
+			}
+		}
+		short[] registerNumber5={(short)1021,(short)1023,(short)1024,(short)1025,(short)1026,(short)1027,(short)1028,(short)1029};
+		int sum3=0;
+		for(int i=0;i<registerNumber.length;i++){
+			sum3=sum+registerNumber5[i];
+			{
+				System.out.println("sum" +sum3);
+			}
+		}
+		
+		
+		long[] phoneNumber={6587225477l,7658345212l,6645734826l,8743435823l,9876775464l,9465566337l,4365229645l,9876756548l};
+		for(int i=phoneNumber.length-1;i>=0;i--){
+			System.out.println(phoneNumber[i]);
+		}
+		
+		long[] phoneNumber1={6587225477l,7658345212l,6645734826l,8743435823l,9876775464l,9465566337l,4365229645l,9876756548l};
+		for(int i=(phoneNumber.length-1)/2;i>=0;i--){
+			System.out.println("middle" +phoneNumber[i]);
+		}
+		long[] phoneNumber2={6587225477l,7658345212l,6645734826l,8743435823l,9876775464l,9465566337l,4365229645l,9876756548l};
+		for(int i=(phoneNumber.length+1)/2;i<phoneNumber.length-1;i++){
+			System.out.println("mid" +phoneNumber[i]);
+		}
+		
+		
+		long[] phoneNumber3={6587225477l,7658345212l,6645734826l,8743435823l,9876775464l,9465566337l,4365229645l,9876756548l};
+		for(int i=(phoneNumber.length+1)/2;i<phoneNumber.length;i++){
+			if(phoneNumber[i]%2==0){
+				System.out.println("even" +phoneNumber[i]);
+		}
+		}
+		
+		long[] phoneNumber4={6587225477l,7658345212l,6645734826l,8743435823l,9876775464l,9465566337l,4365229645l,9876756548l};
+		for(int i=(phoneNumber.length+1)/2;i<phoneNumber.length;i++){
+			if(phoneNumber[i]%2!=0){
+				System.out.println("odd" +phoneNumber[i]);
+		}
+		}
+		
+		long[] phoneNumber5={6587225477l,7658345212l,6645734826l,8743435823l,9876775464l,9465566337l,4365229645l,9876756548l};
+		long sum4=0;
+		for(int i=0;i<phoneNumber5.length;i++){
+			sum4=sum4+phoneNumber5[i];
+			{
+				System.out.println("sum" +sum4);
+			}
+		}
+	}
+	
+}
